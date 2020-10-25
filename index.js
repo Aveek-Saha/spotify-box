94da42b2d57a5d527edcefc13161553b4b3ef753
Saha <aveek.s98@gmail.com>
2020-10-26 01:10:00 +0530

    Create functions

--- index.js
+++ index.js
@@ -13,28 +13,57 @@ var spotifyApi = new SpotifyWebApi({
     refreshToken: refresh_token
 });
 
-spotifyApi.refreshAccessToken().then((data) => {
-        console.log('The access token has been refreshed!');
 
-        // Save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body['access_token']);
-        spotifyApi.getMyTopTracks({time_range: "short_term", limit: 10})
-        .then(function(data) {
+async function getTopTracks() {
+    try {
+        var accessToken = await spotifyApi.refreshAccessToken()
+        spotifyApi.setAccessToken(accessToken.body['access_token']);
+        var topTracks = await spotifyApi.getMyTopTracks({ time_range: "medium_term", limit: 5 })
+        var tracks = topTracks.body.items.map((track) => ({
+            artist: track.artists.map((_artist) => _artist.name)[0],
+            title: track.name
+          }));
+          console.log(tracks);
+    } catch (error) {
+        console.log('Something went wrong!', error);
+    }
+}
 
-            let topTracks = data.body.items.slice(0, 5).map((track) => ({
-                artist: track.artists.map((_artist) => _artist.name)[0],
-                title: track.name
-              }));
-            console.log(topTracks);
+async function getTopArtists() {
+    try {
+        var accessToken = await spotifyApi.refreshAccessToken()
+        spotifyApi.setAccessToken(accessToken.body['access_token']);
+        var topArtists = await spotifyApi.getMyTopArtists({ time_range: "medium_term", limit: 5 })
+        var artists = topArtists.body.items.map((artist) => ({
+            artist: artist.name,
+            popularity: artist.popularity
+          }));
+          console.log(artists);
+    } catch (error) {
+        console.log('Something went wrong!', error);
+    }
+}
 
-    }, (err) => {
-        console.log('Something went wrong!', err);
-    });
-    },(err) => {
 
-        console.log('Could not refresh access token', err);
-    });
+async function getRecentlyPlayed() {
+    try {
+        var accessToken = await spotifyApi.refreshAccessToken()
+        spotifyApi.setAccessToken(accessToken.body['access_token']);
+        var recentlyPlayed = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 5 })
+        var tracks = recentlyPlayed.body.items.map((play) => ({
+            artist: play.track.artists.map((_artist) => _artist.name)[0],
+            title: play.track.name
+          }));
+          console.log(tracks);
+    } catch (error) {
+        console.log('Something went wrong!', error);
+    }
+}
 
 
+(async () => {
+    await getRecentlyPlayed();
+})();
 
-    // https://accounts.spotify.com/authorize?client_id=049c2b23b57c45579678799b71e5fb54&response_type=code&redirect_uri=http://localhost:5000/&scope=user-top-read
+
+// https://accounts.spotify.com/authorize?client_id=049c2b23b57c45579678799b71e5fb54&response_type=code&redirect_uri=http://localhost:5000/&scope=user-top-read
